f3bbbe82ca149b1d9ea23af5c7e5bf9bb7380c2b
Robin Johansson <rjn16002@student.mdh.se>
2020-09-13 16:42:17 +0200

    shadow

--- script.js
+++ script.js
@@ -123,6 +123,32 @@ class Ray
 
         return false;
     }
+
+    // Calculates if any hit, faster for shadows than calculating closest hit
+    calculateIfHit(entities, avoid)
+    {
+        // Test intersection against each entity
+        let hit = false;
+        entities.forEach(entity => 
+        {
+            if (entity != avoid)
+            {
+                var test = entity.intersect(this);
+                if (isNaN(test.min) == false)
+                {
+                    hit = true;
+                    return;
+                }
+            }
+        });
+
+        if (hit)
+        {
+            return true;
+        }
+        
+        return false;
+    }
 }
 
 const LightType= 
@@ -150,6 +176,76 @@ class PointLight extends Light
         super(LightType.POINT, ambient, diffuse, specular);
         this.position = position;
     }
+}
+
+const Texture_Types= {
+    NONE: 0, 
+    IMAGE: 1, 
+    PROCEDURAL: 2 
+}
+
+class Texture
+{
+    constructor(type)
+    {
+        this.type = type;
+    }
+}
+
+class TextureProcedural extends Texture
+{
+    constructor(scale, color1, color2)
+    {
+        super(Texture_Types.PROCEDURAL);
+        this.scale = scale;
+        this.color1 = color1;
+        this.color2 = color2;
+    }
+}
+
+class TextureImage extends Texture
+{
+    constructor()
+    {
+        super(Texture_Types.IMAGE);
+        this.data = null;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    async LoadTexture(filepath)
+    {
+        // Load image
+        let img = new Image();
+        img.src = filepath;
+
+        // This is bad... this function now HAS to be async         
+        await this.LoadImage(img);
+
+        let can = document.createElement("canvas");
+        can.width = img.width;
+        can.height = img.height;
+        let context = can.getContext("2d");
+        context.drawImage(img, 0, 0);
+
+        console.log(img.width);
+        console.log(img.height);
+
+        this.data = context.getImageData(0, 0, can.width, can.height).data;
+        this.width = img.width;
+        this.height = img.height;
+    }
+
+    // Yes... I did...
+    async LoadImage(img)  
+    {
+        return new Promise((resolve, reject) => {
+            img.onload = async () => {
+                console.log("Image Loaded");
+                resolve(true);
+            };
+        });
+    };
 }
 
 class Material 
@@ -230,7 +326,7 @@ class Sphere extends Entity
 
 class Plane extends Entity
 {
-    constructor(center, normal, up, width, height, color, material, texture = false) // (botLeft, topRight, color, material)
+    constructor(center, normal, up, width, height, color, material, texture = null) // (botLeft, topRight, color, material)
     {
         super(center, color, material);
         this.normal = Vec3.vec_normalize(normal);
@@ -293,8 +389,8 @@ class Plane extends Entity
 
                         // Räkna med pixelsize på något sätt :S
                         // Img.width är hur många pixlar texturen har
-                        let pX = Vec3.vec_length(Vec3.vec_cross(Vec3.vec_sub(P, this.topLeft), this.right)) / crossMag;
-                        let pY = Vec3.vec_length(Vec3.vec_cross(Vec3.vec_sub(P, this.topLeft), this.up)) / crossMag;
+                        let pX = Vec3.vec_length(Vec3.vec_cross(Vec3.vec_sub(P, this.topLeft), this.up)) / crossMag;
+                        let pY = Vec3.vec_length(Vec3.vec_cross(Vec3.vec_sub(P, this.topLeft), this.right)) / crossMag;
                         //let pZ = Vec3.vec_length(Vec3.vec_cross(Vec3.vec_sub(P, this.topLeft), this.normal)) / crossMag;
 
                         return {min: t, max: NaN, px: pX, py: pY};
@@ -311,57 +407,48 @@ class Plane extends Entity
         return Vec3.vec_normalize(this.normal);
     }
 
-    loadTexture(texturepath)
-    {
-        // Load image
-        let img = new Image();
-        img.src = texturepath;
-
-        let can = document.createElement("canvas");
-        can.width = img.width;
-        can.height = img.height;
-        let context = can.getContext("2d");
-        context.drawImage(img, 0, 0);
-
-        return context.getImageData(0, 0, can.width, can.height);
-    }
-
     getPixelColor(x, y)
     {
         if (this.texture && isNaN(x) == false && isNaN(y) == false)
         {
-            // Load pixel from texture at image coordinate x,y
-            // DATA: rgbargbargba.. etc..
-            // Modulus för att komma åt "nästa img"
-            //x = x % this.texture.width; 
-            //y = y % this.texture.height;
-            // Flip y-coordinate
-            //y = c.height - y;
-            // Apply the stride (4 elements per pixel)
-            //let index = ((x * 4) + (y * (this.texture.width * 4)));
+            if (this.texture.type == Texture_Types.IMAGE)
+            {
+                // Load pixel from texture at image coordinate x,y
+                let w = this.width; 
+                let h = this.height;
+                // x and y is the distances from the top-left corner. I divide this distance with the width/height of the plane
+                // to get a value between 0 and 1, where 0 is the top-left corner, and 1 is the top right. Then i multiply this value with
+                // the texture width to get the point in this percentage of the image size 
+                let xx = Math.round((x / w) * (this.texture.width - 1)) % (this.texture.width - 1); // p är koordinaten i modelSpace
+                let yy = Math.round((y / h) * (this.texture.height - 1)) % (this.texture.height - 1);
 
-            // Multiply this.color with pixel value from image
-            //return new Color(this.texture.data[index] * this.color.r, this.texture.data[index+1] * this.color.g, this.texture.data[index+2] * this.color.b, this.texture.data[index+3] * this.color.a);
-            let texCol = this.generateTexture(x, y, 1.25, new Color(1.0, 1.0, 1.0, 1.0), new Color(0.0, 0.0, 0.0, 1.0));
-            return new Color(texCol.r * this.color.r, texCol.g * this.color.g, texCol.b * this.color.b, texCol.a * this.color.a);
+                let offset = ((xx * 4) + (yy * (this.texture.width * 4)));
+
+                if (offset > (this.texture.width * this.texture.height *  4))
+                {
+                    offset = (this.texture.width * this.texture.height *  4) - 4;
+                }
+
+                let texCol = new Color(this.texture.data[offset], this.texture.data[offset+1], this.texture.data[offset+2], this.texture.data[offset+3]);
+                return new Color(texCol.r / 255, texCol.g / 255, texCol.b / 255, texCol.a / 255);
+            }
+            else if (this.texture.type == Texture_Types.PROCEDURAL)
+            {
+                let sines = Math.sin(this.texture.scale * x) * Math.sin(this.texture.scale * y);  // * Math.sin(scale * z);
+                if (sines < 0)
+                {
+                    return this.texture.color1;
+                }
+                else
+                {
+                    return this.texture.color2;
+                }
+            }
         }
         else
         {
             // No texture for this obj, return color only
             return this.color;
-        }
-    }
-
-    generateTexture(x, y, scale, color1, color2)
-    {
-        let sines = Math.sin(scale * x) * Math.sin(scale * y);  // * Math.sin(scale * z);
-        if (sines < 0)
-        {
-            return color1;
-        }
-        else
-        {
-            return color2;
         }
     }
 }
@@ -490,7 +577,7 @@ var c = document.getElementById("myCanvas");
 Main();
 
 // FUNCTIONS
-function Main()
+async function Main()
 {
     var ctx = c.getContext("2d");
 
@@ -506,8 +593,30 @@ function Main()
 
     var imageData = new ImageData(imageBufferData, c.width, c.height);
 
+    // Init textures
+    let textures = new Map()
+    textures["apple"] = new TextureImage();
+    await textures["apple"].LoadTexture("./textures/apple.jpg");
+
+    textures["checkerboard"] = new TextureProcedural(20, new Color(1.0, 1.0, 1.0, 1.0), new Color(0.0, 0.0, 0.0, 1.0));
+
+    
+    // Add entities
+    var entities = [];
+    entities.push(new Plane(new Vec3(0.0, -1.0, 25.0), new Vec3(0.0, 1.0, 0.0), new Vec3(0.0, 0.0, 1.0), 50.0, 50.0, new Color(1.0, 1.0, 1.0, 1.0), Materials.CHROME, textures["checkerboard"]));
+    entities.push(new Plane(new Vec3(0.0, 20.0, 50.0), new Vec3(0.0, 0.0, -1.0), new Vec3(0.0, 1.0, 0.0), 50.0, 50.0, new Color(1.0, 1.0, 1.0, 1.0), Materials.CHROME, textures["apple"]));
+    entities.push(new Sphere(new Vec3(0.0, -0.5, 3.0), 0.5, new Color(0.0, 1.0, 0.0, 1.0), Materials.PLASTIC));
+    
+    // Add lights
+    var lights = [];
+    //lights.push(new PointLight(new Vec3(10.0, -4.0, -10.0), new Color(0.0215, 0.1745, 0.0215, 1.0), new Color(0.85, 0.85, 0.85, 1.0), new Color(0.8, 0.8, 0.8, 1.0)));
+    lights.push(new PointLight(new Vec3(10.0, 4.0, -10.0), new Color(1.0, 1.0, 1.0, 1.0), new Color(1.0, 1.0, 1.0, 1.0), new Color(1.0, 1.0, 1.0, 1.0)));
+    
+    // Setup the camera
+    var camera = new Camera(new Vec3(0.0, 0.0, 0.0), new Vec3(0.0, 0.0, 1.0), new Vec3(0.0, 1.0, 0.0), 90, c.width, c.height); //fov ? Math.PI / 2
+    
     // Start Ray tracing
-    Raytrace(c.width, c.height, imageData);
+    Raytrace(c.width, c.height, imageData, entities, lights, camera);
 
     // Draw
     ctx.putImageData(imageData, 0, 0);
@@ -527,25 +636,8 @@ function SetPixel(x, y, buffer, newRed, newGreen, newBlue, newAlpha)
     buffer[index + 3] = newAlpha * 255.0;
 }
 
-function Raytrace(windowWidth, windowHeight, imageBuffer)
+function Raytrace(windowWidth, windowHeight, imageBuffer, entities, lights, camera)
 {
-    // Setup the camera
-    var camera = new Camera(new Vec3(0.0, 0.0, 0.0), new Vec3(0.0, 0.0, 1.0), new Vec3(0.0, 1.0, 0.0), 90, windowWidth, windowHeight); //fov ? Math.PI / 2
-
-    // Add entities
-    var entities = [];
-    entities.push(new Plane(new Vec3(0.0, -5.0, 10.0), new Vec3(0.0, 1.0, 0.0), new Vec3(0.0, 0.0, 1.0), 50.0, 50.0, new Color(1.0, 1.0, 1.0, 1.0), Materials.CHROME, true));
-    entities.push(new Plane(new Vec3(0.0, 5.0, 10.0), new Vec3(0.0, 1.0, 0.0), new Vec3(0.0, 0.0, 1.0), 50.0, 50.0, new Color(1.0, 1.0, 1.0, 1.0), Materials.CHROME, true));
-    entities.push(new Plane(new Vec3(0.0, 0.0, 50.0), new Vec3(0.0, 0.0, -1.0), new Vec3(0.0, 1.0, 0.0), 70.0, 15.0, new Color(1.0, 1.0, 1.0, 1.0), Materials.CHROME, true));
-    entities.push(new Plane(new Vec3(-31.0, 0.0, 10.0), new Vec3(1.0, 0.0, 0.0), new Vec3(0.0, 1.0, 0.0), 70.0, 15.0, new Color(1.0, 1.0, 1.0, 1.0), Materials.CHROME, true));
-    entities.push(new Plane(new Vec3(31.0, 0.0, 10.0), new Vec3(1.0, 0.0, 0.0), new Vec3(0.0, 1.0, 0.0), 70.0, 15.0, new Color(1.0, 1.0, 1.0, 1.0), Materials.CHROME, true));
-    entities.push(new Sphere(new Vec3(0.0, 0.0, 2.0), 0.5, new Color(0.0, 1.0, 0.0, 1.0), Materials.PLASTIC));
-    
-    // Add lights
-    var lights = [];
-    //lights.push(new PointLight(new Vec3(10.0, -4.0, -10.0), new Color(0.0215, 0.1745, 0.0215, 1.0), new Color(0.85, 0.85, 0.85, 1.0), new Color(0.8, 0.8, 0.8, 1.0)));
-    lights.push(new PointLight(new Vec3(10.0, 4.0, -10.0), new Color(1.0, 1.0, 1.0, 1.0), new Color(1.0, 1.0, 1.0, 1.0), new Color(1.0, 1.0, 1.0, 1.0)));
-
     // Run the ray tracing calculations for every pixel
     for (let y = 0; y < camera.windowHeight; y++) 
     {
@@ -557,7 +649,7 @@ function Raytrace(windowWidth, windowHeight, imageBuffer)
             // Test intersection against each entity
             if (ray.calculateClosestHit(entities))
             {
-                var I = ComputeLighting(ray, camera, lights);
+                var I = ComputeLighting(ray, camera, lights, entities);
                 var col = ray.objectHit.getPixelColor(ray.objectHitX, ray.objectHitY);
                 SetPixel(x, y, imageBuffer.data, col.r * I.r, col.g * I.g, col.b * I.b, col.a);
             }
@@ -565,7 +657,7 @@ function Raytrace(windowWidth, windowHeight, imageBuffer)
     }
 }
 
-function ComputeLighting(ray, camera, lights)
+function ComputeLighting(ray, camera, lights, entities)
 {
     var Ip = {r: 0.0, g: 0.0, b: 0.0};
     var Ia = {r: 0.0, g: 0.0, b: 0.0};
@@ -580,51 +672,75 @@ function ComputeLighting(ray, camera, lights)
     var V = Vec3.vec_normalize(Vec3.vec_sub(camera.position, P)); 
 
     lights.forEach(light => {
-        switch (light.type) {
+        switch (light.type) 
+        {
             case LightType.POINT:
-                // Save biggest ambient
-                if (light.ambient.r > Ia.r) {
-                    Ia.r = light.ambient.r;
+                // Is this point visible by this light source?
+                if (CheckShadow(P, light, entities, ray.objectHit) == false)
+                {
+                    // TODO: CHANGE THIS....
+                    // Save biggest ambient 
+                    if (light.ambient.r > Ia.r) {
+                        Ia.r = light.ambient.r;
+                    }
+                    if (light.ambient.g > Ia.g) {
+                        Ia.g = light.ambient.g;
+                    }
+                    if (light.ambient.b > Ia.b) {
+                        Ia.b = light.ambient.b;
+                    }
+                    
+                    // First calculate a vector between the point, and the light
+                    var L = Vec3.vec_normalize(Vec3.vec_sub(light.position, P));
+                    // Then calculate the angle between the surface normal, and the light vector
+                    // If the angle < 0, then we don't add this light since it affects the back of the object
+                    var LN = Math.max(Vec3.vec_dot(N, L), 0.0);
+                    
+                    // Calculate reflection vector
+                    var R = Vec3.vec_normalize(Vec3.vec_sub(Vec3.scalar_mul(2.0 * LN, N), L));
+                    var RV = Math.max(Vec3.vec_dot(R, V), 0.0);
+                    
+                    // Calculate diffuse and specular for each color channel red, green and blue
+                    var diffuseR = ray.objectHit.material.diffuse.r * LN * light.diffuse.r;
+                    var diffuseG = ray.objectHit.material.diffuse.g * LN * light.diffuse.g;
+                    var diffuseB = ray.objectHit.material.diffuse.b * LN * light.diffuse.b;
+                    
+                    var specularR = ray.objectHit.material.specular.r * Math.pow(RV, ray.objectHit.material.shininess) * light.specular.r;
+                    var specularG = ray.objectHit.material.specular.g * Math.pow(RV, ray.objectHit.material.shininess) * light.specular.g; 
+                    var specularB = ray.objectHit.material.specular.b * Math.pow(RV, ray.objectHit.material.shininess) * light.specular.b; 
+                    
+                    Ip.r += diffuseR + specularR;
+                    Ip.g += diffuseG + specularG;
+                    Ip.b += diffuseB + specularB;
                 }
-                if (light.ambient.g > Ia.g) {
-                    Ia.g = light.ambient.g;
-                }
-                if (light.ambient.b > Ia.b) {
-                    Ia.b = light.ambient.b;
-                }
-
-                // First calculate a vector between the point, and the light
-                var L = Vec3.vec_normalize(Vec3.vec_sub(light.position, P));
-                // Then calculate the angle between the surface normal, and the light vector
-                // If the angle < 0, then we don't add this light since it affects the back of the object
-                var LN = Math.max(Vec3.vec_dot(N, L), 0.0);
-
-                // Calculate reflection vector
-                var R = Vec3.vec_normalize(Vec3.vec_sub(Vec3.scalar_mul(2.0 * LN, N), L));
-                var RV = Math.max(Vec3.vec_dot(R, V), 0.0);
-
-                // Calculate diffuse and specular for each color channel red, green and blue
-                var diffuseR = ray.objectHit.material.diffuse.r * LN * light.diffuse.r;
-                var diffuseG = ray.objectHit.material.diffuse.g * LN * light.diffuse.g;
-                var diffuseB = ray.objectHit.material.diffuse.b * LN * light.diffuse.b;
-
-                var specularR = ray.objectHit.material.specular.r * Math.pow(RV, ray.objectHit.material.shininess) * light.specular.r;
-                var specularG = ray.objectHit.material.specular.g * Math.pow(RV, ray.objectHit.material.shininess) * light.specular.g; 
-                var specularB = ray.objectHit.material.specular.b * Math.pow(RV, ray.objectHit.material.shininess) * light.specular.b; 
-                
-                Ip.r += diffuseR + specularR;
-                Ip.g += diffuseG + specularG;
-                Ip.b += diffuseB + specularB;
-                
+                    
                 break;
             default:
                 break;
         }
     });
-
+                
     Ip.r += ray.objectHit.material.ambient.r * Ia.r;
     Ip.g += ray.objectHit.material.ambient.g * Ia.g;
     Ip.b += ray.objectHit.material.ambient.b * Ia.b;
 
     return Ip;
+}
+
+function CheckShadow(point, light, entities, avoid)
+{
+    // Calculate the direction from this point, towards the light
+    let dir = Vec3.vec_normalize(Vec3.vec_sub(light.position, point));
+    // Send a ray from this point, towards the light source
+    let shadowRay = new Ray(point, dir);
+
+    // Does the ray hit something?
+    if (shadowRay.calculateIfHit(entities, avoid) == true)
+    {
+        // In this case the point is not visible for this light source
+        return true;
+    }
+    
+    // The ray didn't hit anything, and is therefore visible for the light
+    return false;
 }
